a1736874f1206315b95dfbcf3b250e2303606203
Michael Camerlynck <71758112+MichaelCamerlynck@users.noreply.github.com>
2022-12-12 13:57:30 +0100

    Update 2_Train_Model.py

--- pages/2_Train_Model.py
+++ pages/2_Train_Model.py
@@ -3,6 +3,7 @@ from fastai.vision.widgets import *
 from fastai.vision.all import *
 from PIL import Image
 import pathlib
+from pathlib import Path
 
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
@@ -18,7 +19,7 @@ st.write("The model has started to train.")
 
 def train(epochs_2, learning_rate_min, learning_rate_max ):
     
-    path = '/app/cheesetypes/AllCheeseData/'
+    path = Path('/app/cheesetypes/AllCheeseData/')
     cheese = DataBlock(
         blocks=(ImageBlock, CategoryBlock), 
         get_items=get_image_files, 
