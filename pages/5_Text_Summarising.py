6e0194a5ea46696886847db7b93e0435c7e16a79
Giedre Zalaite <71758111+GiedreZalaite@users.noreply.github.com>
2022-12-14 20:46:16 +0100

    Add files via upload

---
+++ pages/5_Text_Summarising.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import pipeline
+import streamlit as st
+st.header("Summarising text based on Transformer-based pretrained model")
+value = st.text_area("Enter the text you want to summarise here.")
+button= st.button("Summarise text")
+if button:
+    summarizer = pipeline(
+    "summarization",
+    "pszemraj/long-t5-tglobal-base-16384-book-summary"
+    )
+    result = summarizer(value)
+    st.write(result[0]["summary_text"])
