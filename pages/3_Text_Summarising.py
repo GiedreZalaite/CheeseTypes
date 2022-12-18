d44200c66b51e8b9af07ddd990d8f6ddb0f88bc8
Giedre Zalaite <71758111+GiedreZalaite@users.noreply.github.com>
2022-12-18 17:59:37 +0100

    Update 3_Text_Summarising.py

--- pages/3_Text_Summarising.py
+++ pages/3_Text_Summarising.py
@@ -1,6 +1,9 @@
 import torch
 from transformers import pipeline
 import streamlit as st
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 st.header("Summarising text based on Transformer-based pretrained model")
 value = st.text_area("Enter the text you want to summarise here.")
 button= st.button("Summarise text")
